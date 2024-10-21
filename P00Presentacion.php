16cce87501ca8b3e79c52845f6ad2027d4fcdae6
Axelsanchezz <Galvanaxel492@gmail.com>
2024-10-21 09:32:02 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,14 @@
+<?
+/* CBTIS 89
+P00Presentacion
+Programa que contiene mis datos personales
+Axel Jesus Galvan Sanchez
+3°A Programación T.M.
+*/
+
+echo "Mi nombre es Axel Jesus Galvan Sanchez";
+echo "<br>","Estudio en el CBTIS 89";
+echo "<br>","En tercer semestre de la carrera de Programacion A del turno matutino";
+echo "<br>","Vivo en la ciudad de Durango,Dgo";
+echo "<br>","En la colonia Ignacio Zaragoza";
+?>
