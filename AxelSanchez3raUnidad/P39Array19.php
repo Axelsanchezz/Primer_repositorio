34519001455a992150d06a0511a90c929e67dd3d
Axelsanchezz <Galvanaxel492@gmail.com>
2024-11-08 09:29:05 -0600

    Add files via upload

---
+++ AxelSanchez3raUnidad/P39Array19.php
@@ -0,0 +1,39 @@
+<?php
+/* CBTIS 89
+P39Array19.php
+ Programa que almacena infomación de 6 personas y dependiendo de su edad los contabiliza en diversos arreglos de acuerdo a las siguientes condiciones: si la persona es menor de 18 años se debe colocar 1 en el arreglo $Grupo1 y en los otros dos colocar un 0.Si la persona tiene entre 18 y 49 años se debe colocar un 1 en el arreglo $Grupo2 y en los otros dos colocar un 0.Si la persona tiene 50 años o más se debe colocar un 1 en el arreglo $Grupo3, y en los otros dos un 0.
+
+Axel Jesus Galvan Sanchez
+3°A Programación Matutino */
+
+$Nombre = array("Paco","Mari","Lalo","Rosi","Paty","Beto");
+
+$Edad = array(17,30,71,38,12,58);
+$Grupo1 = array();
+$Grupo2 = array();
+$Grupo3 = array();
+
+for($i=0;$i<=5;$i++)
+	{ if($Edad[$i]<18)
+		{$Grupo1[$i]=1;
+	     $Grupo2[$i]=0;
+	     $Grupo3[$i]=0;}
+      
+        else if($Edad[$i]>=18 and $Edad[$i]<=49)
+        	{$Grupo2[$i]=1;
+        	 $Grupo1[$i]=0;
+        	 $Grupo3[$i]=0;}
+
+            else if($Edad[$i]>=50)
+                 {$Grupo3[$i]=1;
+                  $Grupo1[$i]=0;
+                  $Grupo2[$i]=0;}
+
+}
+
+echo "<br>";
+echo "&nbsp"."CBTIS 89"."<br>";
+ECHO "Nom ". "&nbsp"."  Edad ". " Gpo1 ". " Gpo2 ". " Gpo3 "."<br>";
+for($i=0;$i<6;$i++)
+	{echo $Nombre[$i]." --- ". $Edad[$i]." --- ". $Grupo1[$i]." ---- ". $Grupo2[$i]." ----- ". $Grupo3[$i]. "<br>";}
+?>
